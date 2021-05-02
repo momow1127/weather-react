1d5dfee76a4ab80ebe14f8ca35b77110d233e3d2
momow1127 <wong.mo1127@gmail.com>
2021-05-02 18:52:59 -0400

    Added weather react

---
+++ src/Attribution.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Attribution = () => {
+    return (
+        <div className="Attribution">
+       <a href ="https://github.com/ClaudeManuela/github-netlify-weather-app"> Open source code</a> by <a href="https://github.com/momow1127">Carmen Wong</a>
+            
+        </div>
+    )
+}
+
+export default Attribution
---
+++ src/Search.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Weather.css";
+
+export default function WeatherSearch() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${
+        response.data.weather[0].icon
+      }@2x.png`,
+      description: response.data.weather[0].description
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "094780c710fa4efd669f0df8c3991927";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Search a City" onChange={updateCity} />
+      <button type="Submit">Search</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>
+            City: {city.name}
+          </li>
+          <li>Temperature: {Math.round(weather.temperature)}°F</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
