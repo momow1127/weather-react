65513295f7d0e287860621bbc0f225860d877d6e
momow1127 <wong.mo1127@gmail.com>
2021-05-02 19:00:05 -0400

    Updated Search Code

--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,21 @@
+
 import React, { useState } from "react";
 import axios from "axios";
-import "./Weather.css";
+import "./App.css";
+import
 
-export default function WeatherSearch() {
-  const [city, setCity] = useState("");
-  const [loaded, setLoaded] = useState(false);
-  const [weather, setWeather] = useState({});
+export default function Search() {
+  let [city, setCity] = useState("");
+  let [weather, setWeather] = useState("");
+  let [loaded, setLoaded] = useState(false);
 
-  function displayWeather(response) {
+  function showWeather(response) {
     setLoaded(true);
     setWeather({
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
       humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/${
-        response.data.weather[0].icon
-      }@2x.png`,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description
     });
   }
@@ -23,18 +23,22 @@ export default function WeatherSearch() {
   function handleSubmit(event) {
     event.preventDefault();
     let apiKey = "094780c710fa4efd669f0df8c3991927";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(displayWeather);
+    let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=53a928a54f9cfac5d9a2b1324d145d62&units=metric`;
+    axios.get(apiurl).then(showWeather);
   }
 
-  function updateCity(event) {
+  function changeCity(event) {
     setCity(event.target.value);
   }
 
   let form = (
     <form onSubmit={handleSubmit}>
-      <input type="search" placeholder="Search a City" onChange={updateCity} />
-      <button type="Submit">Search</button>
+      <input
+        type="search"
+        placeholder="Enter a city..."
+        onChange={changeCity}
+      />
+      <input className="search"type="Submit" value="Search" />
     </form>
   );
 
@@ -43,14 +47,11 @@ export default function WeatherSearch() {
       <div>
         {form}
         <ul>
-          <li>
-            City: {city.name}
-          </li>
-          <li>Temperature: {Math.round(weather.temperature)}°F</li>
-          <li>Description: {weather.description}</li>
+          <li className="temperature">{Math.round(weather.temperature)}°C</li>
+          <li>{weather.description}</li>
           <li>Humidity: {weather.humidity}%</li>
           <li>Wind: {weather.wind}km/h</li>
-          <li>
+          <li className="icon">
             <img src={weather.icon} alt={weather.description} />
           </li>
         </ul>
