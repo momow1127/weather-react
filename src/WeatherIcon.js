fda86089ac178e2eebdc8a0b89769527d3900a03
momow1127 <wong.mo1127@gmail.com>
2021-05-09 11:13:47 -0400

    Weather info and icon updated

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,46 +3,35 @@ import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
-
-
 export default function WeatherInfo(props) {
   return (
-        <div className="WeatherInfo">
-        <h1>{props.data.city}</h1>
-        <ul>
-          <li>{props.data.date}</li>
-          <li>{props.data.description}</li>
-        </ul>
-       
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
           <FormattedDate date={props.data.date} />
-          <li className="text-capitalize">{props.data.description}</li>
-       
-
-      <div className="row">
-        <div className="col-8">
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
           <div className="clearfix">
             <div className="float-left">
-            <WeatherIcon code={props.data.icon} alt={props.data.description} />
+              <WeatherIcon code={props.data.icon} size={52} />
             </div>
-            
-            <div className="float-right">
-              <strong>{props.data.temperature}</strong>
-              <span className="units">
-                <a href="/">°C</a> | <a href="/">°F</a>
-              </span>
+
+            <div className="float-left">
+              <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
-        <div className="col-12">
-          <div className="navcontainer">
-            <ul>
-              <li>Humidity: {props.data.humidity}%</li>
-              <li>Wind: {props.data.wind} km/h</li>
-            </ul>
-          </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
+          </ul>
         </div>
       </div>
     </div>
-
   );
 }
