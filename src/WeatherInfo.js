cef87d300d92be83cb31b3d21dafdc2b4a67a213
momow1127 <wong.mo1127@gmail.com>
2021-05-13 19:41:40 -0400

    Update code

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,35 +3,46 @@ import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
 
+
+
 export default function WeatherInfo(props) {
   return (
-    <div className="WeatherInfo">
-      <h1>{props.data.city}</h1>
-      <ul>
-        <li>
+        <div className="WeatherInfo">
+        <h1>{props.data.city}</h1>
+        <ul>
+          <li>{props.data.date}</li>
+          <li>{props.data.description}</li>
+        </ul>
+       
           <FormattedDate date={props.data.date} />
-        </li>
-        <li className="text-capitalize">{props.data.description}</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
+          <li className="text-capitalize">{props.data.description}</li>
+       
+
+      <div className="row">
+        <div className="col-8">
           <div className="clearfix">
             <div className="float-left">
-              <WeatherIcon code={props.data.icon} size={52} />
+            <WeatherIcon code={props.data.icon} alt={props.data.description} />
             </div>
-
-            <div className="float-left">
-              <WeatherTemperature celsius={props.data.temperature} />
+            
+            <div className="float-right">
+              <strong>{props.data.temperature}</strong>
+              <span className="units">
+                <a href="/">°C</a> | <a href="/">°F</a>
+              </span>
             </div>
           </div>
         </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind} km/h</li>
-          </ul>
+        <div className="col-12">
+          <div className="navcontainer">
+            <ul>
+              <li>Humidity: {props.data.humidity}%</li>
+              <li>Wind: {props.data.wind} km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
+
   );
 }
