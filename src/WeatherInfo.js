301b5169919b093831a645b8aaec250fd6df6733
momow1127 <wong.mo1127@gmail.com>
2021-05-16 11:47:29 -0400

    update weatherinfo

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -13,17 +13,17 @@ export default function WeatherInfo(props) {
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
-        <div className="col-8">
+        <div className="col-6">
           <div className="clearfix">
             <div className="float-left">
               <WeatherIcon code={props.data.icon} size={52} />
             </div>
-            <div className="float-right">
+            <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
         </div>
-        <div className="col-12">
+        <div className="col-6">
           <ul>
             <li>
               Humidity
