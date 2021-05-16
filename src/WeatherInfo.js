49088b7cb6a3701294e5256303b0ae0fe05c3283
momow1127 <wong.mo1127@gmail.com>
2021-05-16 12:09:07 -0400

    Update weatherinfo js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
+
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
@@ -26,12 +27,8 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-6">
           <ul>
-            <li>
-              Humidity:{props.data.humidity}%{" "}
-            </li>
-            <li>
-              Wind:{props.data.wind} km/h{" "}
-            </li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
           </ul>
         </div>
       </div>
